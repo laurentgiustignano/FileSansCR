008c0ff7545e7c2b43f88446c8b1ce8ffce51f58
Laurent Giustignano <laurent.giustignano@free.fr>
2022-04-14 08:49:36 +0200

    Affiche taille des fichiers in/out

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@ int main() {
     std::string nomFichierSrc;
     std::string nomFichierDst;
     char byte = 0;
+    unsigned long nombreLu = 0, nombreEcrit = 0;
 
     std::cout << "Donnez le nom du fichier source : ";
     std::cin >> nomFichierSrc;
@@ -25,13 +26,18 @@ int main() {
         throw ; //EXIT_FAILURE;
     }
 
+
     while (fileLecture.get(byte)) {
+        nombreLu++;
         if(byte != 0x0D) {  // different Carrier Return
             fileEcriture.put(byte);
+            nombreEcrit++;
         }
     }
 
-    std::cout << "Fin " << std::endl;
+    std::cout << "Taille fichier origine :  "<< nombreLu << std::endl;
+    std::cout << "Taille fichier destination :  "<< nombreEcrit << std::endl;
+    std::cout << nombreLu - nombreEcrit <<" octet(s) ont été supprimé." << std::endl;
     fileLecture.close();
     fileEcriture.close();
 
